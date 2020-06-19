c35d4e764e17a635b047329f8b2a1316d664cf98
Charm Srisuthapan <40420605+csrisuth@users.noreply.github.com>
2020-06-19 14:26:18 +0700

    Add comment, remove redundancy code.

--- kafka_pyspark.py
+++ kafka_pyspark.py
@@ -1,9 +1,20 @@
+'''
+Bitmex Kafka Consumer using pyspark
+
+Author: Charm Srisuthapan
+Date: Jun 19, 2020
+'''
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from pyspark.sql.types import *
 from pyspark.sql.functions import col, to_timestamp, unix_timestamp, substring, from_json
 from decimal import Decimal
+
+# Apache Kafka Setting
+__HOSTNAME = "localhost:9092"
+__KAFKA_TOPIC = "bitmex01"
 
 spark = SparkSession \
     .builder \
@@ -13,10 +24,13 @@ spark = SparkSession \
 df_raw = spark \
   .readStream \
   .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "bitmex01") \
+  .option("kafka.bootstrap.servers", __HOSTNAME) \
+  .option("subscribe", __KAFKA_TOPIC) \
   .option("startingOffsets", "latest") \
   .load()
+
+# Due to inferSchema mismatch in pyspark, some decimal data eg. 9400.0 may consider as int, 
+# all data were read as string then convert to proper datatype in next step
 
 schema = StructType([
     StructField('timestamp', StringType(), True),
@@ -32,10 +46,11 @@ schema = StructType([
 ])
 
 data = df_raw.select(from_json(col("value").cast("string"), schema).alias("tmp"))
-data.printSchema()
+#data.printSchema()
 
-data2 = data.select("tmp.timestamp")
-
+# Convert data into proper datatype
+# Note: Due to microseconds issue while cast string to timestamp, 
+# microsecond need to be add after convert date/time to timestamp
 df = data.withColumn("timestamp", (unix_timestamp("tmp.timestamp", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").cast("long")+\
                                  col("tmp.timestamp").substr(21,3).cast("float")/1000.0).cast("timestamp"))\
                                 .withColumn("symbol",col("tmp.symbol"))\
@@ -50,62 +65,23 @@ df = data.withColumn("timestamp", (unix_timestamp("tmp.timestamp", "yyyy-MM-dd'T
 
 df = df.drop("tmp")
 
-df_tradeHist = df
-df_tradeHist.createOrReplaceTempView("trade_history")
+df_tradeHist = df.createOrReplaceTempView("trade_history")
 
-aggInterval = "1 minutes"
-
-def createSQLStatement(interval):
-    switcher = {
-        "15 seconds" : ",cast(concat(date(timestamp),' ',hour(timestamp),':',minute(timestamp),':',floor(second(timestamp)/15)*15) as timestamp) as group_column",
-        "30 seconds" : ",cast(concat(date(timestamp),' ',hour(timestamp),':',minute(timestamp),':',floor(second(timestamp)/30)*30) as timestamp) as group_column",
-        "1 minutes"  : ",cast(concat(date(timestamp),' ',hour(timestamp),':',minute(timestamp)) as timestamp) as group_column",
-        "5 minutes"  : ",cast(concat(date(timestamp),' ',hour(timestamp),':',floor(minute(timestamp)/5)*5) as timestamp) as group_column",
-        "15 minutes" : ",cast(concat(date(timestamp),' ',hour(timestamp),':',floor(minute(timestamp)/15)*15) as timestamp) as group_column",
-        "1 hours"  : ",cast(concat(date(timestamp),' ',hour(timestamp),':00') as timestamp) as group_column",
-        "4 hours"  : ",cast(concat(date(timestamp),' ',floor(hour(timestamp)/4)*4,':00') as timestamp) as group_column",
-        "6 hours"  : ",cast(concat(date(timestamp),' ',floor(hour(timestamp)/6)*6,':00') as timestamp) as group_column",
-        "12 hours" : ",cast(concat(date(timestamp),' ',floor(hour(timestamp)/12)*12,':00') as timestamp) as group_column",
-        "1 days"  : ",cast(concat(date(timestamp),' ','00:00') as timestamp) as group_column"
-    }
-    return switcher.get(interval, "Please input correct interval")
-
-df_group_temp = spark.sql('SELECT timestamp, symbol, price, size '+ \
-                             createSQLStatement(aggInterval)+ \
-                             ' FROM trade_history')
-
-df_group_temp.createOrReplaceTempView("hist_group")
-df_interval = spark.sql('''SELECT group_column as timestamp,
-                           symbol,
-                           first(price) as open, 
-                           max(price) as high, 
-                           min(price) as low, 
-                           last(price) as close,
-                           sum(size) as volumn
-                           FROM hist_group
-                           group by symbol, group_column
-                           ''')
-
+# Print output to console for monitoring
 query1 = df.writeStream.outputMode("Append") \
-            .trigger(processingTime='60 seconds') \
+            .trigger(processingTime='15 seconds') \
             .format("console") \
             .option("truncate","false") \
             .start()
 
+# Write tick-data to disk (for use in graph plot)
 query2 = df.writeStream.format("csv") \
-    .trigger(processingTime='5 minutes') \
+    .trigger(processingTime='1 minutes') \
     .option("format", "append") \
     .option("path", "./output/tick_data/destination") \
     .option("checkpointLocation", "./output/tick_data/checkpoint") \
     .outputMode("append") \
     .start()
 
-query3 = df_interval.writeStream.outputMode("Update") \
-            .trigger(processingTime=aggInterval) \
-            .format("console") \
-            .option("truncate","false") \
-            .start()
-
 query1.awaitTermination()
 query2.awaitTermination()
-query3.awaitTermination()
